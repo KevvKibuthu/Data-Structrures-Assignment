c4608845781935934903055788d608c0da975b81
KevvKibuthu <kelvinmuthee43@gmail.com>
2026-02-16 22:43:41 +0300

    Create tree.py

---
+++ tree.py
@@ -0,0 +1,14 @@
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+# Create tree
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+
+print("Root:", root.data)
+print("Left child:", root.left.data)
+print("Right child:", root.right.data)
