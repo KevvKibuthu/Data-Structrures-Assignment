4138659185f8b7918b8e4622edd684cc272f78c2
KevvKibuthu <kelvinmuthee43@gmail.com>
2026-02-16 22:37:55 +0300

    Create primtive.py

---
+++ primtive.py
@@ -0,0 +1,15 @@
+# Integer
+age = 20
+print("Integer:", age)
+
+# Float
+height = 5.9
+print("Float:", height)
+
+# String
+name = "Kibuthu"
+print("String:", name)
+
+# Boolean
+is_student = True
+print("Boolean:", is_student)
